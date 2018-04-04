4b19ec64a9b8a784c2b5029c37cdf543c3f83c5a
Michael Bartlett <bartlettmic@gmail.com>
2018-04-04 13:17:51 -0600

    test new vizualizer

--- index.js
+++ index.js
@@ -30,62 +30,40 @@ Object.assign(canvas.style, {
 
 document.body.appendChild(canvas);
 
-analyser.fftSize = 512
+analyser.fftSize = 256
 var bufferLength = analyser.frequencyBinCount
 var data = new Uint8Array(bufferLength)
+    // ctx.lineWidth = 0.5
+var cap = data.length * 5 / 9
+var start = parseInt(cap / 10, 10)
+cap -= start
+var lines_per = 5
+var last_timestamp = Date.now()
+var dots = []
+for (var i = 0; i < cap; i++) dots.push(new Dot((i / cap) * 360))
 
-// ctx.lineWidth = 0.5
-// var cap = data.length * 5 / 9
-// var start = parseInt(cap / 10, 10)
-// cap -= start
-// var lines_per = 5
-// var last_timestamp = Date.now()
-// var dots = []
-// for (var i = 0; i < cap; i++) dots.push(new Dot((i / cap) * 360))
 
-
-// function Dot(hue) {
-//     this.max_vol = 127
-//     this.hue = hue
-//     this.radius = 0
-//     this.theta = 0
-//     this.x = 0
-//     this.y = 0
-//     this.rect = function() {
-//         this.x = this.radius * Math.cos(this.theta)
-//         this.y = this.radius * Math.sin(this.theta)
-//     }
-// }
-
-// function ellipse(theta, a, b) {
-//     return ((a / 2) * (b / 2)) / Math.sqrt(Math.pow((b / 2) * Math.sin(theta), 2) + Math.pow((a / 2) * Math.cos(theta), 2))
-// }
-
-function spectrum(width, scale) {
-    const halfWidth = Math.floor(canvas.width / 2)
-    const halfHeight = Math.floor(canvas.height / 2)
-    ctx.beginPath()
-    for (let horizontal = 1; horizontal > -2; horizontal -= 2) {
-        for (let vertical = 1; vertical > -2; vertical -= 2) {
-            ctx.moveTo(halfWidth, halfHeight)
-            for (let i = 0; i < bufferLength; i++) {
-                let rawHeight = data[i] / 256
-                rawHeight = Math.pow(rawHeight, 5)
-                const height = (halfHeight * rawHeight) * vertical
-                ctx.lineTo(halfWidth + (width / 2 * i) * horizontal, halfHeight - height * scale)
-            }
-        }
+function Dot(hue) {
+    this.max_vol = 127
+    this.hue = hue
+    this.radius = 0
+    this.theta = 0
+    this.x = 0
+    this.y = 0
+    this.rect = function() {
+        this.x = this.radius * Math.cos(this.theta)
+        this.y = this.radius * Math.sin(this.theta)
     }
-    ctx.closePath()
-    ctx.fill()
+}
+
+function ellipse(theta, a, b) {
+    return ((a / 2) * (b / 2)) / Math.sqrt(Math.pow((b / 2) * Math.sin(theta), 2) + Math.pow((a / 2) * Math.cos(theta), 2))
 }
 
 (canvas.draw = function() {
     var timestamp = Date.now()
-        // ctx.save();
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
+    ctx.save();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     // ctx.translate(canvas.width/2, canvas.height/2);
     // ctx.rotate((Date.now()%1000)/500*Math.PI)
     // ctx.fillRect(-100,-100,200,200)
@@ -100,62 +78,46 @@ function spectrum(width, scale) {
 
 
     analyser.getByteFrequencyData(data)
-        // draw previous frame
-    ctx.drawImage(canvas, -10, 0, canvas.width + 20, canvas.height)
-        // darken it
-    ctx.globalCompositeOperation = 'multiply'
-    // ctx.fillStyle = '#EEE'
-    // ctx.fillRect(0, 0, canvas.width, canvas.height)
-    const width = parseInt(bufferLength * 5 / 6, 10)
-    const scaledWidth = ((canvas.width - width) / width)
-    ctx.globalCompositeOperation = 'source-over'
-    ctx.fillStyle = 'hsl(' + timestamp / 60 + ',100%,50%)'
-    spectrum(scaledWidth, 1)
-    ctx.fillStyle = '#000'
-    spectrum(scaledWidth, 0.6)
+        // ctx.drawImage(canvas, -10, 0, canvas.width + 20, canvas.height)
+    ctx.translate(canvas.width / 2, canvas.height / 2)
+    ctx.rotate(Math.PI * 3 / 2)
+    const minrad = Math.min(canvas.height, canvas.width) / 6
+    for (var i = 0; i < cap - 1; i++) {
+        if (data[i + start] > dots[i].max_vol) dots[i].max_vol = data[i + start]
+        else if (timestamp - last_timestamp >= 50 && dots[i].max_vol > 1) {
+            dots[i].max_vol *= 0.999
+            last_timestamp = timestamp
+        }
+        dots[i].theta = ((i + 1) / (cap)) * Math.PI * 2 + timestamp / 8000
+            // const minrad = ellipse(dots[i].theta, canvas.width / 2, canvas.height / 2)
+        const maxrad = ellipse(dots[i].theta, canvas.width, canvas.height) - minrad
+        dots[i].radius = minrad + (data[i + start] / dots[i].max_vol) * maxrad
+        dots[i].rect()
+        dots[i].hue += 0.25
+    }
+    for (var i = 0; i < cap - 1; i++) {
+        for (var j = 1; j < lines_per; j++) {
+            const index = parseInt((j * cap / lines_per + i) % cap, 10)
+            ctx.beginPath()
+                // ctx.arc(dots[i].x, dots[i].y, 2, 0, 2 * Math.PI)
+            ctx.moveTo(dots[i].x, dots[i].y)
+            ctx.quadraticCurveTo(0, 0, dots[index].x, dots[index].y)
+            const grd = ctx.createLinearGradient(dots[i].x, dots[i].y, dots[index].x, dots[index].y)
+            grd.addColorStop(0, 'hsl(' + dots[i].hue + ', 100%,50%)')
+            grd.addColorStop(1, 'hsl(' + dots[index].hue + ', 100%,50%)')
 
+            ctx.strokeStyle = grd
+                // ctx.strokeText(i, dots[i].x, dots[i].y)
 
-    // analyser.getByteFrequencyData(data)
-    //     // ctx.drawImage(canvas, -10, 0, canvas.width + 20, canvas.height)
-    // ctx.translate(canvas.width / 2, canvas.height / 2)
-    // ctx.rotate(Math.PI * 3 / 2)
-    // const minrad = Math.min(canvas.height, canvas.width) / 6
-    // for (var i = 0; i < cap - 1; i++) {
-    //     if (data[i + start] > dots[i].max_vol) dots[i].max_vol = data[i + start]
-    //     else if (timestamp - last_timestamp >= 50 && dots[i].max_vol > 1) {
-    //         dots[i].max_vol *= 0.999
-    //         last_timestamp = timestamp
-    //     }
-    //     dots[i].theta = ((i + 1) / (cap)) * Math.PI * 2 + timestamp / 8000
-    //         // const minrad = ellipse(dots[i].theta, canvas.width / 2, canvas.height / 2)
-    //     const maxrad = ellipse(dots[i].theta, canvas.width, canvas.height) - minrad
-    //     dots[i].radius = minrad + (data[i + start] / dots[i].max_vol) * maxrad
-    //     dots[i].rect()
-    //     dots[i].hue += 0.25
-    // }
-    // for (var i = 0; i < cap - 1; i++) {
-    //     for (var j = 1; j < lines_per; j++) {
-    //         const index = parseInt((j * cap / lines_per + i) % cap, 10)
-    //         ctx.beginPath()
-    //             // ctx.arc(dots[i].x, dots[i].y, 2, 0, 2 * Math.PI)
-    //         ctx.moveTo(dots[i].x, dots[i].y)
-    //         ctx.quadraticCurveTo(0, 0, dots[index].x, dots[index].y)
-    //         const grd = ctx.createLinearGradient(dots[i].x, dots[i].y, dots[index].x, dots[index].y)
-    //         grd.addColorStop(0, 'hsl(' + dots[i].hue + ', 100%,50%)')
-    //         grd.addColorStop(1, 'hsl(' + dots[index].hue + ', 100%,50%)')
-
-    //         ctx.strokeStyle = grd
-    //             // ctx.strokeText(i, dots[i].x, dots[i].y)
-
-    //         ctx.stroke()
-    //         ctx.closePath()
-    //     }
-    // }
+            ctx.stroke()
+            ctx.closePath()
+        }
+    }
 
 
 
-    // ctx.restore();
-    // window.requestAnimationFrame(canvas.draw);
+    ctx.restore();
+    window.requestAnimationFrame(canvas.draw);
 })();
 
 
